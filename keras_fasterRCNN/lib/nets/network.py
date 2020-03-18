e3548d2c5789fadc4498a5d9b9555b728612b754
QiuSYang <37255500+QiuSYang@users.noreply.github.com>
2020-03-18 21:39:59 +0800

    Add files via upload
    "update new content"

--- keras_fasterRCNN/lib/nets/network.py
+++ keras_fasterRCNN/lib/nets/network.py
@@ -51,7 +51,11 @@ class NetWork(object):
             self._anchor_component()
             # region proposal network (RPN 网络)
             rois = self._region_proposal(net_conv, is_training, initializer)
-            pass 
+            # region of interest pooling
+        if True: # cfg.POOLING_MODE == 'crop':
+            pool5 = self._crop_pool_layer(net_conv, rois, "pool5")
+        else:
+            raise NotImplementedError
     
     # 为特征图生产anchors
     def _anchor_component(self):
@@ -261,6 +265,25 @@ class NetWork(object):
             self._score_summaries.update(self._proposal_targets)
 
         return rois, roi_scores
+    
+    def _crop_pool_layer(self, bottom, rois, name):
+        with tf.variable_scope(name) as scope:
+            batch_ids = tf.squeeze(tf.slice(rois, [0, 0], [-1, 1], name="batch_id"), [1])
+            # Get the normalized coordinates of bounding boxes
+            bottom_shape = tf.shape(bottom)
+            height = (tf.to_float(bottom_shape[1]) - 1.) * np.float32(self._feat_stride[0])
+            width = (tf.to_float(bottom_shape[2]) - 1.) * np.float32(self._feat_stride[0])
+            x1 = tf.slice(rois, [0, 1], [-1, 1], name="x1") / width
+            y1 = tf.slice(rois, [0, 2], [-1, 1], name="y1") / height
+            x2 = tf.slice(rois, [0, 3], [-1, 1], name="x2") / width
+            y2 = tf.slice(rois, [0, 4], [-1, 1], name="y2") / height
+            # Won't be back-propagated to rois anyway, but to save time
+            bboxes = tf.stop_gradient(tf.concat([y1, x1, y2, x2], axis=1))
+            pre_pool_size = cfg.POOLING_SIZE * 2
+            crops = tf.image.crop_and_resize(bottom, bboxes, tf.to_int32(batch_ids), [pre_pool_size, pre_pool_size], name="crops")
+
+        # return slim.max_pool2d(crops, [2, 2], padding='SAME')
+        return KL.MaxPool2D(pool_size=(2, 2), padding='same')(crops)
 
     def create_architecture(self, mode, num_classes, tag=None, 
                             anchor_scales=(8, 16, 32), anchor_ratios=(0.5, 1, 2)):
