36bf3a98e2de08ee06d23a26f2013f0bee5c27f4
QiuSongYang <15962230327@163.com>
2020-04-07 22:46:31 +0800

    "add tensorflow2 resnet"

---
+++ tensorflow2_faster_rcnn/detectron/models/backbones/resnet.py
@@ -0,0 +1,8 @@
+"""
+# resnet model for tensorflow2
+# Reference:
+# - [Deep Residual Learning for Image Recognition](
+#     https://arxiv.org/abs/1512.03385)
+"""
+import tensorflow as tf
+from tensorflow.keras import layers
