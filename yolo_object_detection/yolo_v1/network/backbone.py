ff2d53389315f6b598c9593fcc3ebd0ee5643831
QiuSongYang <15962230327@163.com>
2020-06-04 23:03:58 +0800

    "add first codes"

---
+++ yolo_object_detection/yolo_v1/network/backbone.py
@@ -0,0 +1,17 @@
+"""
+# yolo v1 主干网络构建
+"""
+import os
+import logging
+import torch
+from torch import nn
+
+_logger = logging.getLogger(__name__)
+
+
+class YoloV1Net(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
