dc595f8b9c24dbc0e6843dc21f7bec1728a13a5e
杨球松 <yangqiusong@cloudwalk.cn>
2021-01-11 19:02:12 +0800

    "update transformer"

--- basic_network/torch_transformer_self-attention_paper_reproduction/transformer/layers.py
+++ basic_network/torch_transformer_self-attention_paper_reproduction/transformer/layers.py
@@ -37,8 +37,50 @@ class ScaledDotProductAttention(nn.Module):
 
 class MultiHeadAttention(nn.Module):
     """Multi-Head Attention module"""
-    def __init__(self):
-        pass
+    def __init__(self, n_head, d_model, d_k, d_v, dropout=0.1):
+        super(MultiHeadAttention, self).__init__()
+        self.n_head = n_head
+        self.d_k = d_k
+        self.d_v = d_v
+
+        self.w_qs = nn.Linear(d_model, self.n_head * self.d_k, bias=False)  # 多头权重一起初始化
+        self.w_ks = nn.Linear(d_model, self.n_head * self.d_k, bias=False)
+        self.w_vs = nn.Linear(d_model, self.n_head * self.d_v, bias=False)
+        self.fc = nn.Linear(self.n_head * self.d_v, d_model, bias=False)
+
+        self.attention = ScaledDotProductAttention(temperature=d_k ** 0.5)
+
+        self.dropout = nn.Dropout(dropout)
+        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+
+    def forward(self, q, k, v, mask=None):
+        d_k, d_v, n_head = self.d_k, self.d_v, self.n_head
+        sz_b, len_q, len_k, len_v = q.size(0), q.size(1), k.size(1), v.size(1)
+        residual = q
+
+        # Pass through the pre-attention projection: b x lq x (n*dv)
+        # Separate different heads: b x lq x nh x dv
+        q = self.w_qs(q).view(sz_b, len_q, n_head, d_k)
+        k = self.w_ks(k).view(sz_b, len_k, n_head, d_k)
+        v = self.w_vs(v).view(sz_b, len_v, n_head, d_v)
+
+        # Transpose for attention dot product: b x nh x lq x dv
+        q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)  # 矩阵乘法只对最后两维操作
+
+        if mask is not None:
+            mask = mask.unsqueeze(1)  # For head axis broadcasting
+
+        q, attn = self.attention(q, k, v, mask=mask)
+
+        # Transpose to move the head dimension back: b x lq x nh x dv
+        # Combine the last two dimensions to concatenate all the heads together: b x lq x (nh*dv)
+        q = q.transpose(1, 2).contiguous().view(sz_b, len_q, -1)  # 多头直接concat
+        q = self.dropout(self.fc(q))
+        q += residual
+
+        q = self.layer_norm(q)
+
+        return q, attn
 
 
 class PositionwiseFeedForward(nn.Module):
